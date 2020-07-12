69f5fe5860e910d910aa89d7fb61ab47c19e99a3
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-12 03:20:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma (phrase)
     return "NO, NOT SINCE 1938!"
     elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else phrase == "WHAT?"
+  else phrase == ph&& rase.downcase != "WHAT?"
     return "HUH? ! SPEAK UP, SONNY!"
   end
 end
